330c6f78901fd63472ed03f70a344a2eea2b993b
TimCurtis468 <tim_r_curtis@hotmail.com>
2023-11-18 10:41:53 +0000

    Player immune during collision
    https://www.youtube.com/watch?v=0x57N5Iy_jY

--- player/player.gd
+++ player/player.gd
@@ -12,6 +12,10 @@ signal healthChanged
 
 @export var knockbackPower: int = 500
 
+var isHurt: bool = false
+
+var enemyCollisions = []
+
 func _ready():
 	effects.play("RESET")
 
@@ -35,30 +39,39 @@ func UpdateAnimation():
 
 		animations.play("walk" + direction)		
 	
-func handleCollision():
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var _collider = collision.get_collider()
+#func handleCollision():
+#	for i in get_slide_collision_count():
+#		var collision = get_slide_collision(i)
+#		var _collider = collision.get_collider()
 		
 	
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
-	handleCollision()
+#	handleCollision()
 	UpdateAnimation()
+	if isHurt == false:
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
+
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
 
 
 func _on_hurt_box_area_entered(area):
 	if area.name == "hitBox":
-		currentHealth -= 1
-		if currentHealth < 0:
-			currentHealth = maxHealth
-		healthChanged.emit(currentHealth)
-		knockback(area.get_parent().velocity)
-		effects.play("hurtBlink")
-		hurtTimer.start()
-		await hurtTimer.timeout
-		effects.play("RESET")
+		enemyCollisions.append(area)
+
 
 func knockback(enemyVelocity):
 	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
@@ -70,3 +83,7 @@ func knockback(enemyVelocity):
 	move_and_slide()
 	print_debug(position)
 	print_debug(" ")
+
+
+func _on_hurt_box_area_exited(area):
+	enemyCollisions.erase(area)
